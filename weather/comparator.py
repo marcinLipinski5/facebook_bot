ec3a167a7271f701b589d7246ed24a36a412582f
Marcin Lipinski <38397437+marcinLipinski5@users.noreply.github.com>
2021-11-05 10:58:19 +0100

    Update comparator.py

--- weather/comparator.py
+++ weather/comparator.py
@@ -7,11 +7,11 @@ class Comparator:
     def __init__(self):
         # TODO optimize this
         self.__weather_forecast_list = ApiCalls().get_weather_forecast_list()
-        self.__person_requirements = {"Kasia":
+        self.__person_requirements = {"A":
                                           {'temp_min': 5,
                                            'temp_max': 1000000,
                                            'main_weather': ['Clear']},
-                                      "Marcin":
+                                      "B":
                                           {'temp_min': -1000000,
                                            'temp_max': 28,
                                            'main_weather': []}
@@ -21,29 +21,29 @@ class Comparator:
         self.compare_all_days()
 
     def compare_temp_min(self, day_forecast: Dict):
-        if self.__person_requirements['Kasia']['temp_min'] >= day_forecast['temp_min']:
-            self.__compare_answer += f'Too cold for Kasia\n'
+        if self.__person_requirements['A']['temp_min'] >= day_forecast['temp_min']:
+            self.__compare_answer += f'Too cold for A\n'
             self.__positive_result = False
-        elif self.__person_requirements['Marcin']['temp_min'] >= day_forecast['temp_min']:
-            self.__compare_answer += f'Too cold for Marcin\n'
+        elif self.__person_requirements['B']['temp_min'] >= day_forecast['temp_min']:
+            self.__compare_answer += f'Too cold for B\n'
             self.__positive_result = False
 
     def compare_temp_max(self, day_forecast: Dict):
-        if self.__person_requirements['Kasia']['temp_max'] <= day_forecast['temp_max']:
-            self.__compare_answer += f'Too hot for Kasia\n'
+        if self.__person_requirements['A']['temp_max'] <= day_forecast['temp_max']:
+            self.__compare_answer += f'Too hot for A\n'
             self.__positive_result = False
-        elif self.__person_requirements['Marcin']['temp_max'] <= day_forecast['temp_max']:
-            self.__compare_answer += f'Too hot for Marcin\n'
+        elif self.__person_requirements['B']['temp_max'] <= day_forecast['temp_max']:
+            self.__compare_answer += f'Too hot for B\n'
             self.__positive_result = False
 
     def compare_weather_status(self, day_forecast: Dict):
-        for weather_condition in self.__person_requirements['Kasia']['main_weather']:
+        for weather_condition in self.__person_requirements['A']['main_weather']:
             if day_forecast['main_weather'] not in weather_condition:
-                self.__compare_answer += f'Weather condition {day_forecast["main_weather"]} is not suitable for Kasia'
+                self.__compare_answer += f'Weather condition {day_forecast["main_weather"]} is not suitable for A'
                 self.__positive_result = False
-        for weather_condition in self.__person_requirements['Marcin']['main_weather']:
+        for weather_condition in self.__person_requirements['B']['main_weather']:
             if day_forecast['main_weather'] not in weather_condition:
-                self.__compare_answer += f'Weather condition {day_forecast["main_weather"]} is not suitable for Marcin'
+                self.__compare_answer += f'Weather condition {day_forecast["main_weather"]} is not suitable for B'
                 self.__positive_result = False
 
     def compare_all_days(self):
